b03a0f6db2985503618437944e32f7c8a68ff2fa
Alexandr Makarov <notengine@gmail.com>
2019-10-06 20:25:50 +0300

    initial commit

--- src/test.php
+++ src/test.php
@@ -4,13 +4,19 @@ require __DIR__  . '/../vendor/autoload.php';
 
 $query = 'cat memes';
 $google = new \Chabberwock\Siteinfo\GoogleQuery();
-$links = $google->run($query, 11);
-$result = [];
-foreach ($links as $link) {
-    $p = parse_url($link);
-    $domain = $p['host'];
-    $siteInfo = new Chabberwock\Siteinfo\SiteInfo($domain);
-    $result[] = ['link'=>$link, 'domainExpires'=>$siteInfo->domainExpires, 'certExpires'=>$siteInfo->certExpires];
+try {
+    $links = $google->run($query, 11);
+    $result = [];
+    foreach ($links as $link) {
+        $p = parse_url($link);
+        $domain = $p['host'];
+        $siteInfo = new Chabberwock\Siteinfo\SiteInfo($domain);
+        $result[] = ['link'=>$link, 'domainExpires'=>$siteInfo->domainExpires, 'certExpires'=>$siteInfo->certExpires];
+    }
+    var_dump($result);
+    
+} catch (Exception $e) {
+    echo 'Failed: ' . $e->getMessage() . "\n";
 }
-var_dump($result);
+
 
